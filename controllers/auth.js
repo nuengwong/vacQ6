4d7803e348be1ea64c85359b0390b035392c19de
Nuengwong T <nuengwong.t@cp.eng.chula.ac.th>
2022-02-02 16:00:05 +0700

    Chapter08 Slide11

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,6 +3,22 @@ const User = require('../models/User');
 //@desc     Register user
 //@route    POST /api/v1/auth/register
 //@access   Public
-exports.register=(req,res,next)=>{
-    res.status(200).json({success:true});
+exports.register=async (req,res,next)=>{
+    try {
+        const {name, email, password, role}=req.body;
+
+        //Create user
+        const user=await User.create({
+            name,
+            email,
+            password,
+            role
+        });
+        res.status(200).json({success:true});
+
+    } catch (error) {
+        res.status(400).json({success:false});
+        console.log(error.stack);
+        
+    }
 }
