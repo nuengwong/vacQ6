ffefa043fa878a508efd7e7db7cd7f8c0a23f1bc
Nuengwong T <nuengwong.t@cp.eng.chula.ac.th>
2022-02-02 16:21:12 +0700

    Chapter08 Slide24

--- controllers/auth.js
+++ controllers/auth.js
@@ -25,3 +25,34 @@ exports.register=async (req,res,next)=>{
         
     }
 }
+
+//@desc     Login user
+//@route    POST /api/v1/auth/register
+//@access   Public
+exports.login=async (req,res,next)=>{
+    const {email, password}=req.body;
+
+    //Validate email&password
+    if(!email || !password){
+        return res.status(400).json({success:false, msg:'Please provide an email and password'});
+    }
+
+    //Check for user
+    const user=await User.findOne({email}).select('+password');
+    
+    if(!user){
+        return res.status(400).json({success:false, msg:'Invalid credentials'});
+    }
+
+    //Check if password matches
+    const isMatch=await user.matchPassword(password);
+
+    if(!isMatch){
+        return res.status(401).json({success:false, msg:'Invalid credentials'});
+    }
+
+    //Create token
+    const token=user.getSignedJwtToken();
+
+    res.status(200).json({success:true, token});
+}
