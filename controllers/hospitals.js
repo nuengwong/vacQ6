5cdecc3b63061027705f6fe623934f644401cfb8
Nuengwong T <nuengwong.t@cp.eng.chula.ac.th>
2022-02-02 11:37:05 +0700

    Chapter07.02 Slide#15, but cannot send get request to postman

--- controllers/hospitals.js
+++ controllers/hospitals.js
@@ -1,3 +1,5 @@
+const Hospital=require('../models/Hospital');
+
 exports.getHospitals=(req,res,next)=>{
     res.status(200).json({success:true, msg:'Show all hospitals'});
 }
@@ -7,6 +9,7 @@ exports.getHospital=(req,res,next)=>{
 }
 
 exports.createHospital=(req,res,next)=>{
+    console.log(req.body);
     res.status(200).json({success:true, msg:'Create hospital'});
 }
 
--- server.js
+++ server.js
@@ -1,15 +1,29 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const connectDB=require('./config/db');
+
 //Route files
 const hospitals = require('./routes/hospitals');
 
 //Load env vars
 dotenv.config({path:'./config/config.env'});
+//Connect to DB
+connectDB();
 
 const app=express();
+
+//Body parser
+app.use(express.json);
 
 //Mount routers
 app.use('/api/v1/hospitals',hospitals);
 
 const PORT=process.env.PORT || 5000;
-app.listen(PORT, console.log('Server running in ',process.env.NODE_ENV, ' mode on port ',PORT));
+const server = app.listen(PORT, console.log('Server running in ',process.env.NODE_ENV, ' mode on port ',PORT));
+
+//Handler unhandled promise rejections
+process.on('unhandleRejection', (err,promise)=>{
+    console.log(`Error: ${err.message}`);
+    //Close server & exit process
+    server.close(()=>process.exit(1));
+});
