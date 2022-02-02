32f70b2723638b40563818c6767e23417a62a0ba
Nuengwong T <nuengwong.t@cp.eng.chula.ac.th>
2022-02-02 21:04:15 +0700

    Chapter08 Done

--- routes/hospitals.js
+++ routes/hospitals.js
@@ -3,9 +3,9 @@ const {getHospitals, getHospital, createHospital, updateHospital, deleteHospital
 
 const router = express.Router();
 
-const {protect}=require('../middleware/auth');
+const {protect,authorize}=require('../middleware/auth');
 
-router.route('/').get(getHospitals).post(protect,createHospital);
-router.route('/:id').get(getHospital).put(protect,updateHospital).delete(protect,deleteHospital);
+router.route('/').get(getHospitals).post(protect,authorize('admin'),createHospital);
+router.route('/:id').get(getHospital).put(protect,authorize('admin'),updateHospital).delete(protect,authorize('admin'),deleteHospital);
 
 module.exports=router;
