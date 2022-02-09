47ab1639ddf00d50fe54a15ebc36af1b3ee247b1
Nuengwong T <nuengwong.t@cp.eng.chula.ac.th>
2022-02-09 13:35:35 +0700

    Chapter09 Slide#07

--- controllers/hospitals.js
+++ controllers/hospitals.js
@@ -2,10 +2,34 @@ const Hospital=require('../models/Hospital');
 
 exports.getHospitals=async (req,res,next)=>{
     let query;
+
+    //Copy req.query
+    const reqQuery={...req.query};
+
+    //Fields to exclude
+    const removeFields=['select','sort'];
+
+    //Loop over remove fields and delete them from reqQuery
+    removeFields.forEach(param=>delete reqQuery[param]);
+    console.log(reqQuery);
+
     let queryStr=JSON.stringify(req.query);
     queryStr=queryStr.replace(/\b(gt|gte|lt|lte|in)\b/g,match=>`$${match}`);
     console.log(queryStr);
     query=Hospital.find(JSON.parse(queryStr));
+
+    //Select Fields
+    if(req.query.select){
+        const fields=req.query.select.split(',').join(' ');
+        query=query.select(fields);
+    }
+    //Sort
+    if(req.query.sort){
+        const sortBy=req.query.sort.split(',').join(' ');
+        query=query.sort(sortBy);
+    } else{
+        query=query.sort('-createdAt');
+    }
 
     try {
         const hospitals = await query;
