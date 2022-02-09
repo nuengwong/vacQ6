4fb93546c82ca040bce2b54a2fbdf9158cca0faa
Nuengwong T <nuengwong.t@cp.eng.chula.ac.th>
2022-02-09 13:58:09 +0700

    Chapter09 Slide017

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const connectDB=require('./config/db');
 //Route files
 const hospitals = require('./routes/hospitals');
 const auth = require('./routes/auth');
+const appointments =require('./routes/appointments');
+
 
 //Load env vars
 dotenv.config({path:'./config/config.env'});
@@ -23,6 +25,7 @@ app.use(cookieParser());
 //Mount routers
 app.use('/api/v1/hospitals',hospitals);
 app.use('/api/v1/auth',auth);
+app.use('/api/v1/appointments', appointments);
 
 const PORT=process.env.PORT || 5000;
 const server = app.listen(PORT, console.log('Server running in ',process.env.NODE_ENV, ' mode on port ',PORT));
