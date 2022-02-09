b062406d9d0ba818e8f27b92d6caa5e90c2cd15b
Nuengwong T <nuengwong.t@cp.eng.chula.ac.th>
2022-02-09 14:45:19 +0700

    Chapter09 Done before Test

--- controllers/appointments.js
+++ controllers/appointments.js
@@ -73,6 +73,17 @@ exports.addAppointment=async (req,res,next)=>{
                return res.status(404).json({success:false,message:`No hospital with the id of ${req.params.hospitalId}`});        
             }
     
+            //add user Id to req.body
+            req.body.user=req.user.id;
+
+            //Check for existed appointment
+            const existedAppointments=await Appointment.find({user:req.user.id});
+
+            //If the user is not an admin, they can only create 3 appointment.
+            if(existedAppointments.length >= 3 && req.user.role !== 'admin'){
+                return res.status(400).json({success:false,message:`The user with ID ${req.user.id} has already made 3 appointments`});      
+            }          
+
             const appointment = await Appointment.create(req.body);
     
             res.status(200).json({
@@ -96,6 +107,12 @@ exports.updateAppointment=async (req,res,next)=>{
         if(!appointment){
             return res.status(404).json({success:false,message:`No appointment with the id of ${req.params.id}`});   
         }
+
+        //Make sure user is the appointment owner
+        if(appointment.user.toString()!== req.user.id && req.user.role !== 'admin'){
+               return res.status(401).json({success:false,message:`User ${req.user.id} is not authorized to update this appointment`});   
+        }
+
 
         appointment=await Appointment.findByIdAndUpdate(req.params.id,req.body,{
             new:true,
@@ -122,7 +139,11 @@ exports.deleteAppointment=async (req,res,next)=>{
             if(!appointment){
                 return res.status(404).json({success:false,message:`No appointment with the id of ${req.params.id}`});  
             }
-    
+            
+            //Make sure user is the appointment owner
+            if(appointment.user.toString()!== req.user.id && req.user.role !== 'admin'){
+                return res.status(401).json({success:false,message:`User ${req.user.id} is not authorized to update this appointment`});   
+            }
             await appointment.remove();
     
             res.status(200).json({
