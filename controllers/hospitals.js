dfd4a249a56f9160694b5d9350b6eead8fb846c8
Nuengwong T <nuengwong.t@cp.eng.chula.ac.th>
2022-02-09 13:43:33 +0700

    Chapter09 Slide09

--- controllers/hospitals.js
+++ controllers/hospitals.js
@@ -7,7 +7,7 @@ exports.getHospitals=async (req,res,next)=>{
     const reqQuery={...req.query};
 
     //Fields to exclude
-    const removeFields=['select','sort'];
+    const removeFields=['select','sort','page','limit'];
 
     //Loop over remove fields and delete them from reqQuery
     removeFields.forEach(param=>delete reqQuery[param]);
@@ -32,8 +32,28 @@ exports.getHospitals=async (req,res,next)=>{
     }
 
     try {
+
+        //Pagination
+        const page=parseInt(req.query.page,10)||1;
+        const limit=parseInt(req.query.limit,10)||25;
+        const startIndex=(page-1)*limit;
+        const endIndex=page*limit;
+        const total=await Hospital.countDocuments();
+
+        query=query.skip(startIndex).limit(limit);
+
         const hospitals = await query;
-        
+
+        //Pagination Result
+        const pagination={};
+
+        if(endIndex<total){
+            pagination.next={page:page+1,limit}
+        }
+
+        if(startIndex>0){
+            pagination.prev={page:page-1,limit}
+        }        
 
         res.status(200).json({success:true, count:hospitals.length, data:hospitals});
         
